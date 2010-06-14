f31ffa1d286d371722a025881df48a3504d0e557
marutanm <marutanm@gmail.com>
2010-06-14 21:21:48 +0900

    Added ClockViewController

---
+++ Classes/ClockViewController.h
@@ -0,0 +1,16 @@
+//
+//  ClockViewController.h
+//  QuarterClock
+//
+//  Created by marutanm on 6/14/10.
+//  Copyright 2010 shisobu.in. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface ClockViewController : UIViewController {
+
+}
+
+@end
