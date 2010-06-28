1fe7c77a85fc0467e1a72d62b234a90daf4c5f3c
marutanm <marutanm@gmail.com>
2010-06-28 19:04:03 +0900

    Implemented nextLabel

--- Classes/DigitView.m
+++ Classes/DigitView.m
@@ -10,6 +10,8 @@
 
 #import "DigitView.h"
 
+#define CURRENT 50
+#define NEXT 60
 
 @implementation DigitView
 
@@ -17,14 +19,21 @@
 - (id)initWithFrame:(CGRect)frame {
     if (self = [super initWithFrame:frame]) {
         UILabel *label = [self label:frame];
+        label.tag = CURRENT;
         [self addSubview:label];
         [label release];
+
+        UILabel *nextLabel = [self label:CGRectMake(frame.origin.x, frame.origin.y + frame.size.height, frame.size.width, frame.size.height)];
+        nextLabel.tag = NEXT;
+        [self addSubview:nextLabel];
+        [nextLabel release];
     }
     return self;
 }
 
 - (UILabel *)label:(CGRect)frame {
-    UILabel *label = [[[UILabel alloc] initWithFrame:frame] autorelease];
+    UILabel *label = [[UILabel alloc] initWithFrame:frame];
+    // UILabel *label = [[[UILabel alloc] initWithFrame:frame] autorelease];
     label.backgroundColor = [UIColor blackColor];
     label.textColor = [UIColor whiteColor];
     label.font = [UIFont systemFontOfSize:500];
@@ -35,9 +44,28 @@
 }
 
 - (void)text:(NSString *)text {
-    for (UILabel *label in self.subviews) {
-        label.text = text;
-    }
+    [[self viewWithTag:CURRENT] setText:text];
+    [[self viewWithTag:NEXT] setText:@"X"];
+
+    // for (UILabel *label in self.subviews) {
+        // label.text = text;
+    // }
+}
+
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
+    NSLog(@"Start: %s", __func__);
+    CGPoint currenCenter = self.center;
+
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    [UIView beginAnimations:nil context:context];
+    [UIView setAnimationDuration:10.0f];
+    [UIView setAnimationCurve:UIViewAnimationCurveEaseInOut];
+    [UIView setAnimationDidStopSelector:@selector(endAnimation)];
+
+    self.center = CGPointMake(currenCenter.x, currenCenter.y - 480);
+
+    [UIView commitAnimations];	
+
 }
 
 - (void)dealloc {
