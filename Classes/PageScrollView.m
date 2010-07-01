47ad9233e0403c36c36eccc451152c2ff2415289
marutanm <marutanm@gmail.com>
2010-07-02 00:46:09 +0900

    Implemented absolutely terrible codes to animate each digit
    too ugly...

--- Classes/PageScrollView.m
+++ Classes/PageScrollView.m
@@ -72,9 +72,16 @@
     NSLog(@"%d", sec);
     if (sec >= 50) {
         [[scrollView viewWithTag:TAG_OFFSET + 3] slideUpDigit:10.0];
-        // if ([[currentTimeArray objectAtIndex:3] isEqualToString:@"9"]) {
-          if ([[currentTimeArray objectAtIndex:3] integerValue] == [[scrollView viewWithTag:TAG_OFFSET + 3] maxValue]) {
-                   [[scrollView viewWithTag:TAG_OFFSET + 2] slideUpDigit:10.0];
+        if ([[currentTimeArray objectAtIndex:3] integerValue] == [[scrollView viewWithTag:TAG_OFFSET + 3] maxValue]) {
+                    [[scrollView viewWithTag:TAG_OFFSET + 2] slideUpDigit:10.0];
+                    if ([[currentTimeArray objectAtIndex:2] integerValue] == [[scrollView viewWithTag:TAG_OFFSET + 2] maxValue]) {
+                                [[scrollView viewWithTag:TAG_OFFSET + 1] slideUpDigit:10.0];
+                                if ([[currentTimeArray objectAtIndex:1] integerValue] == [[scrollView viewWithTag:TAG_OFFSET + 1] maxValue]) {
+                                            [[scrollView viewWithTag:TAG_OFFSET + 0] slideUpDigit:10.0];
+                                } else if (([[scrollView viewWithTag:TAG_OFFSET + 0] integerValue] == 2) && ([[scrollView viewWithTag:TAG_OFFSET +1] integerValue] == 3)) {
+                                            [[scrollView viewWithTag:TAG_OFFSET + 0] slideUpDigit:10.0];
+                                }
+                    }
         }
     }
 
